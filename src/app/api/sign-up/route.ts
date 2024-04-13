113e8d116b256e8c24b49283162b946268a12716
saif01010 <saiful.12006518@lpu.in>
2024-04-14 00:08:50 +0530

    Add sign-up API route and functionality

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -2,11 +2,67 @@ import UserModel from "@/models/user.model";
 import { sendEmailToken } from "@/helpers/sendEmailToken";
 import dbConnect from "@/lib/dbConnect";
 import bycrypt from "bcryptjs";
+import { use } from "react";
 
 export async function POST (req:Request,res:Response){
     await dbConnect();
    
     try {
+      const {email,username,password} = await req.json();
+
+      const userByUsername = await UserModel.findOne({username,isVarified:true});
+      if(userByUsername){
+        return Response.json(
+            {
+              success: false,
+              message: 'Username already exists.',
+            },
+            { status: 400 }
+          );
+      }
+
+      const userByEmail = await UserModel.findOne({email});
+      let token = Math.floor(100000 + Math.random() * 900000).toString();
+
+      if(userByEmail){
+        if(userByEmail.isVarified){
+            return Response.json(
+                {
+                  success: false,
+                  message: 'User already exists.',
+                },
+                { status: 400 }
+              );
+            }else{
+                const hashedPassword =  bycrypt.hashSync(password,10);
+                userByEmail.verifyToken = token;
+                userByEmail.verifyTokenExpires = new Date(Date.now() + 3600000);
+                userByEmail.password = hashedPassword;
+                await userByEmail.save(); 
+            }
+      }else{
+        const hashedPassword = bycrypt.hashSync(password,10);
+        const newUser = new UserModel({
+            email,
+            username,
+            password: hashedPassword,
+            verifyToken: token,
+            verifyTokenExpires: new Date(Date.now() + 3600000)
+        });
+        await newUser.save();
+      }
+
+     const emailResponse = await sendEmailToken(email,username,token);
+      if(!emailResponse.success){
+        return Response.json(
+            {
+              success: false,
+              message: emailResponse.message,
+            },
+            { status: 400 }
+          );
+      }
+
         return Response.json(
             {
               success: true,
