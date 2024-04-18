87f783cedf2e7ccff2add29a4eda2b6bf78d3f7d
saif01010 <saiful.12006518@lpu.in>
2024-04-18 22:26:23 +0530

    Add middleware.ts file for route protection

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+export {default} from 'next-auth/middleware';
+import {NextRequest, NextResponse} from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+
+export const config = {
+    matcher:['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*']
+}
+export async function middleware(req: NextRequest, res: NextResponse) {
+  const token = await getToken({ req , secret: process.env.NEXTAUTH_SECRET});
+  const url = req.nextUrl;
+  if (token && (url.pathname.startsWith('sing-in') ||
+   url.pathname.startsWith('sign-up')||
+   url.pathname.startsWith('verify')||
+   url.pathname.startsWith('/'))) {
+    return NextResponse.redirect(new URL('/dashboard', req.url));
+  }
+  if (!token && !url.pathname.startsWith('/dashboard')) {
+        return NextResponse.redirect(new URL('/sign-in', req.url));
+  }
+    return NextResponse.next();
+}
+
