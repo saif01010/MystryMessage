0f2df3a2375cf734e0a4f96947e6c2bd7c1fad29
saif01010 <saiful.12006518@lpu.in>
2024-04-18 22:26:09 +0530

    Add AuthProvider component to layout.tsx

---
+++ src/context/AuthProvider.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { SessionProvider } from "next-auth/react"
+
+export default function AuthProvider({
+    children,
+}: { readonly children: React.ReactNode }) {
+    return (
+        <SessionProvider>
+            {children}
+        </SessionProvider>
+    )
+}
