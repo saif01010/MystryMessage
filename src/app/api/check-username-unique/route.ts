097ac2d7a29eb422df851eb678d9fe3d2dc852c1
saif01010 <saiful.12006518@lpu.in>
2024-04-18 23:57:09 +0530

    Update POST method to GET method in check-username-unique route

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -7,7 +7,7 @@ const usernameSchema = z.object({
     username:usernameValidation
 })
 
-export  async function POST(req:Request){
+export  async function GET(req:Request){
     await dbConnect();
     try {
         const {searchParams} = new URL(req.url); 
