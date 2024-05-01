78a67cfda4ee1bfd7516c11bc9a7333042fce8ec
saif01010 <saiful.12006518@lpu.in>
2024-05-01 23:53:55 +0530

    Refactor Navbar component styles and add logout button variant

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ import { Button } from "./ui/button"
             <Link className='text-xl font-bold ml-0 ' href="/dashboard">Mystry Message</Link>
             {session ?(<>
             <span className="mr-4">{user.username || user.email}</span>
-            <Button onClick={()=>(signOut())} className="w-full md:w-auto bg-slate-100 text-black" variant={'outline'}>LogOut</Button></>)
+            <Button onClick={()=>(signOut())} className=" w-full md:w-auto bg-slate-300 text-black" variant={'outline'}>LogOut</Button></>)
             :(<Link href='sign-in'>
                 <Button className="w-full md:w-auto bg-slate-100 text-black" variant={'outline'}>Login</Button>
             </Link>)
